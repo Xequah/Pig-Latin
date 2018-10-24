91e00ae797b456756480e77289f24841452c0a44
charles robertson and glen sale <glen@>
2018-10-24 16:24:16 -0700

    it will translate a sentence to a pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,16 @@ var latintranslate = function(vowel){
 
  // debugger;
   var output = "";
-  if (vowels.includes(vowel[0])) {
+  if  (vowel [0] ==="q" && vowel [1] ==="u"){
+    output += vowel.substring(2, vowel.length) + vowel[0] + vowel[1] + "ay";
+
+  }else if (vowels.includes(vowel[0])) {
     output += vowel.substring(0, vowel.length) + "way";
   } else {
-    output += vowel.substring(0, vowel.length) + "ay";
+    output += vowel.substring(1, vowel.length) + vowel[0] + "ay";
+
   }
-  console.log(output);
+  //console.log(output);
   return output;
 }
 
@@ -16,8 +20,14 @@ var latintranslate = function(vowel){
 $(document).ready(function(){
   $("#translate").submit(function(event){
     var piglatin = ($("#words").val());
-    var phrase = latintranslate(piglatin);
+    var inputArray = piglatin.split(' ');
+    var phrase = "";
+    inputArray.forEach(function(word) {
+      phrase += latintranslate(word) + " ";
+    });
+    // var phrase = latintranslate(piglatin);
     $("#output").text(phrase);
     event.preventDefault();
+
   });
 });
